1a45f1c3b78d43e6fb1f7ab9860484b423d0d702
Aswathyvk <aswathyvk832@gmail.com>
2026-06-03 18:53:54 +0530

    Fix production auth API URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API="http://localhost:5000/api/auth";
+const API="https://goodibaddi.onrender.com/api/auth";
 
 export const signup=(data)=>{
     return axios.post(
